2ab5f46b6ff4001c5e10c4aae69fc9041604fa48
Romain DEROCLE <romain.derocle@manomano.com>
2018-11-20 21:01:01 +0100

    wip

---
+++ src/string/exo4/code.js
@@ -0,0 +1,17 @@
+import 'colors'
+import Assert from '../../lib/assert'
+
+/**
+ * Manipulation des chaines de caractères
+ */
+
+// recherche une sous-chaine dans une chaine de caractère
+
+const string = 'ma super Chaine de Caractère'
+
+const find = (str, subString) => {
+    
+}
+
+console.log('Test: recherche chaine'.blue)
+Assert.equals(find(string, 'Chaine'), 9)
