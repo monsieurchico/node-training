350db61494bd595fa656cd9008f57a9ed786fae6
Romain DEROCLE <romain.derocle@manomano.com>
2018-11-20 23:49:12 +0100

    wip

---
+++ src/object/exo16/code.js
@@ -0,0 +1,22 @@
+import 'colors'
+import Assert from '../../lib/assert'
+
+/**
+ * Manipulation des objets
+ */
+
+/*
+ * Créer une fonction qui parcourt un tableau et affiche ses infos comme dans l'exemple :
+ *
+ * const user = { id: 12, name: 'Romain', city: 'Bordeaux' }
+ * display(user)
+ *   id => 12
+ *   name => Romain
+ *   city => Bordeaux
+ */
+
+const display = (id, name, address, zipcode, city) => {
+    
+}
+
+display({ id: 12, name: 'Romain', city: 'Bordeaux' })
