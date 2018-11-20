0e2f581f550e7fe9ee861f62aa43b5d28dc7da8c
Romain DEROCLE <romain.derocle@manomano.com>
2018-11-20 22:58:32 +0100

    wip

---
+++ src/array/exo9/code.js
@@ -0,0 +1,43 @@
+import 'colors'
+import Assert from '../../lib/assert'
+
+/**
+ * Manipulation des tableaux
+ */
+
+/*
+Parcourir un tableau et afficher ses éléments
+*/
+
+/*
+Utiliser boucle for
+*/
+const loopWithFor = (array) => {
+    
+}
+
+/*
+Utiliser Array.forEach
+*/
+const loopWithForeach = (array) => {
+    
+}
+
+/*
+Utiliser Array.map
+*/
+const loopWithMap = (array) => {
+    
+}
+
+console.log('loopWithFor'.blue)
+loopWithFor([1, 345, 'toto', 34])
+console.log(' ')
+
+console.log('loopWithForeach'.blue)
+loopWithForeach([1, 345, 'toto', 34])
+console.log(' ')
+
+console.log('loopWithMap'.blue)
+loopWithMap([1, 345, 'toto', 34])
+console.log(' ')
