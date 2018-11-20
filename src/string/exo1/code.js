607da066a5a79851f600975c334b59e5c932e401
Romain DEROCLE <romain.derocle@manomano.com>
2018-11-20 22:39:49 +0100

    wip

--- src/string/exo1/code.js
+++ src/string/exo1/code.js
@@ -12,5 +12,4 @@ const string = 'ma super Chaine de Caractère'
 const convertToLowerCase = (str) => {
 }
 
-console.log('Test: convert to string to lower case'.blue)
 Assert.equals(convertToLowerCase(string), 'ma super chaine de caractère')
