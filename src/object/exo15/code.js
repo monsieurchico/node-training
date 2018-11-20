8fe9252e2e038753a8778d34de6133c8ca4dd56f
Romain DEROCLE <romain.derocle@manomano.com>
2018-11-20 23:51:53 +0100

    fix

--- src/object/exo15/code.js
+++ src/object/exo15/code.js
@@ -6,8 +6,9 @@ import Assert from '../../lib/assert'
  */
 
 /*
+ * @see Object.entries
  * Créer une fonction qui parcourt un tableau et affiche ses infos comme dans l'exemple :
- *
+ * 
  * const user = { id: 12, name: 'Romain', city: 'Bordeaux' }
  * display(user)
  *   id => 12
@@ -16,7 +17,9 @@ import Assert from '../../lib/assert'
  */
 
 const display = (object) => {
-    
+    Object.entries(object).map(elem => {
+        console.log(`${elem[0]} => ${elem[1]}`)
+    })
 }
 
 display({ id: 12, name: 'Romain', city: 'Bordeaux' })
