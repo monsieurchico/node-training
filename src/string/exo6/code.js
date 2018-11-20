029ba44ad7011fc9e6788127a3123b8bb98af94a
Romain DEROCLE <romain.derocle@manomano.com>
2018-11-20 22:19:18 +0100

    wip

--- src/string/exo6/code.js
+++ src/string/exo6/code.js
@@ -19,7 +19,18 @@ String.fromCharCode(122) => z
 */
 
 const toUpperCase = (str) => {
+    let newString = ''
+    for (let i = 0 ; i < str.length ; i++) {
+        const letter = str[i]
+        const code = letter.charCodeAt(0)
+        let newLetter = letter
+        if (code >= 97 && code <= 122) {
+            newLetter = String.fromCharCode(code - 32)
+        }
+        newString = newString.concat(newLetter)
+    }
 
+    return newString
 }
 
 console.log('Test: to upper case string'.blue)
