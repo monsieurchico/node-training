d88966a7ba87f2197a456c4c08a7c09753a0c687
Romain DEROCLE <romain.derocle@manomano.com>
2018-11-20 21:48:30 +0100

    wip

--- src/string/exo5/code.js
+++ src/string/exo5/code.js
@@ -5,13 +5,15 @@ import Assert from '../../lib/assert'
  * Manipulation des chaines de caractères
  */
 
-// remplacer une portion de chaine par une autre
+// concatener des chaines de caracteres
 
-const string = 'ma super Chaine de Caractère'
-
-const replace = (str, search, replacement) => {
-
+const addString = (str1, str2, str3) => {
+    
 }
 
 console.log('Test: replace string'.blue)
-Assert.equals(replace(string, 'Chaine', 'liste'), 'ma super liste de Caractère')
+Assert.equals(addString(
+    'ma super',
+    'Chaine',
+    'de caractères'
+), 'ma super Chaine de caractères')
