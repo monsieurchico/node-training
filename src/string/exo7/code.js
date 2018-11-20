1a543b47278ed1a8d38b913d32f287f9932e8fa4
Romain DEROCLE <romain.derocle@manomano.com>
2018-11-20 22:23:52 +0100

    wip

---
+++ src/string/exo7/code.js
@@ -0,0 +1,24 @@
+import 'colors'
+import Assert from '../../lib/assert'
+
+/**
+ * Manipulation des chaines de caractères
+ */
+
+/*
+Chaines dynamiques
+
+Compléter la fonction suivante afin qu'elle retourne le lien suivant :
+
+<a href="http://monlien">mon contenu</a>
+*/
+
+const linkify = (href, content) => {
+    
+}
+
+console.log('Test: to upper case string'.blue)
+Assert.equals(
+    linkify('https://www.google.fr', 'Go to Google'),
+    '<a href="https://www.google.fr">Go to Google</a>'
+)
