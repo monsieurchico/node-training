67ca5f95c0bc5bd29260257f280ac42dc9f5758f
Romain DEROCLE <romain.derocle@manomano.com>
2018-11-20 20:19:15 +0100

    add exo 2

---
+++ src/string/exo2/code.js
@@ -0,0 +1,17 @@
+import 'colors'
+import Assert from '../../lib/assert'
+
+/**
+ * Manipulation des chaines de caractères
+ */
+
+// passer une chaine de caractère en majuscule
+
+const string = 'ma super Chaine de Caractère'
+
+const convertToUpperCase = (str) => {
+
+}
+
+console.log('Test: convert string to upper case'.blue)
+Assert.equals(convertToUpperCase(string), 'MA SUPER CHAINE DE CARACTÈRE')
