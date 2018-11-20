98b7d8ed3fd081966f67f31afc260a21be4e519b
Romain DEROCLE <romain.derocle@manomano.com>
2018-11-20 21:05:02 +0100

    wip

---
+++ src/string/exo5/code.js
@@ -0,0 +1,17 @@
+import 'colors'
+import Assert from '../../lib/assert'
+
+/**
+ * Manipulation des chaines de caractères
+ */
+
+// remplacer une portion de chaine par une autre
+
+const string = 'ma super Chaine de Caractère'
+
+const replace = (str, subString) => {
+    
+}
+
+console.log('Test: replace string'.blue)
+Assert.equals(find(string, 'Chaine'), 9)
