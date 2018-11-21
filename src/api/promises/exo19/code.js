927f300cf3309e614add23449b28dea24a7864e8
Romain DEROCLE <romain.derocle@manomano.com>
2018-11-21 12:13:33 +0100

    wip

--- src/api/promises/exo19/code.js
+++ src/api/promises/exo19/code.js
@@ -10,7 +10,18 @@ import fetch from 'node-fetch'
  * Décomposer la reponse de fetch pour extraire les headers et une partie du body
  */
 const callUrl = async (url) => {
-    
+    const res = await fetch(url)
+    const body = res.json()
+    console.log(body)
+    return {
+        headers: {
+            code: 200,
+            'content-type': 'application/json'
+        },
+        body: {
+            userId
+        }
+    }
 }
 
 const test = async () => {
