f6c7bf1f4b14ebe5d8b795e5f83d4454dd12605b
Romain DEROCLE <romain.derocle@manomano.com>
2018-11-21 12:07:04 +0100

    wip

--- src/api/promises/exo18/code.js
+++ src/api/promises/exo18/code.js
@@ -9,12 +9,12 @@ import fetch from 'node-fetch'
 /*
  * Appeler une URL avec fetch et async / await
  */
-const callUrlWithPromise = async (url, callback) => {
+const callUrl = async (url, callback) => {
 
 }
 
 const test = async () => {
-    const response = await callUrlWithPromise('https://jsonplaceholder.typicode.com/todos/1')
+    const response = await callUrl('https://jsonplaceholder.typicode.com/todos/1')
     Assert.equals(response.userId, 1)
 }
 
